a05187e5a83b766476cc03b4be6e825abd7fb502
UserIsAdi <adiauliarahmn@gmail.com>
2023-10-09 13:48:27 +0800

    Update

--- app/Database/Migrations/2023-10-09-033456_CreateTabelReview.php
+++ app/Database/Migrations/2023-10-09-033456_CreateTabelReview.php
@@ -6,39 +6,39 @@ use CodeIgniter\Database\Migration;
 
 class CreateTabelReview extends Migration
 {
-    public function up()
-    {
-        $this->forge->addfield([
-          'id_review'=> [
-            'type'            => 'BIGINT',
-            'constraint'      => 20,
-            'unsigned'        => true,
-            'auto_increment'  => true,
-           ],
+  public function up()
+  {
+    $this->forge->addfield([
+      'id_review' => [
+        'type'            => 'BIGINT',
+        'constraint'      => 20,
+        'unsigned'        => true,
+        'auto_increment'  => true,
+      ],
 
-          'id_sewa' => [
-            'type'            => 'BIGINT',
-            'constraint'      => 20,
-            'unsigned'        => true,
-          ],
+      'id_sewa' => [
+        'type'            => 'BIGINT',
+        'constraint'      => 20,
+        'unsigned'        => true,
+      ],
 
-          'rate' => [
-            'type'            => 'INT',
-            'constraint'      => 11,
-            'default'         => 0,
-          ],
-          'review' => [
-            'type'            => 'TEXT'
-          ],
-        ]);
+      'rate' => [
+        'type'            => 'INT',
+        'constraint'      => 11,
+        'default'         => 0,
+      ],
+      'review' => [
+        'type'            => 'TEXT'
+      ],
+    ]);
 
-       $this->forge->addKey('id_review', true);
-       $this->forge->addForeignKey('id_sewa','tb_sewa', 'id_sewa', 'CASCADE', 'CASCADE');
-       $this->forge->createTable('tb_review');
-    }
+    $this->forge->addKey('id_review', true);
+    $this->forge->addForeignKey('id_sewa', 'tb_sewa', 'id_sewa', 'CASCADE', 'CASCADE');
+    $this->forge->createTable('tb_review');
+  }
 
-    public function down()
-    {
-        $this->forge->dropTable('tb_review');
-    }
+  public function down()
+  {
+    $this->forge->dropTable('tb_review');
+  }
 }
